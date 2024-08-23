6537d602b8ab9cec07f68a1b4090898dffb46370
Mikail Katsiev <105316523+katsiev1997@users.noreply.github.com>
2024-08-24 01:02:09 +0300

    Interceptors

--- src/conception/guard.ts
+++ src/conception/guard.ts
@@ -4,6 +4,7 @@ import { Injectable, CanActivate, ExecutionContext, UnauthorizedException } from
 export class AuthGuard implements CanActivate {
     canActivate(context: ExecutionContext): boolean {
         const request = context.switchToHttp().getRequest();
+        console.log("Guard")
         const isAuth = request.headers.authorization === "secret";
         if (!isAuth) {
             throw new UnauthorizedException("Not authorized");
--- src/flowers/flowers.controller.ts
+++ src/flowers/flowers.controller.ts
@@ -1,9 +1,11 @@
-import { Controller, Get, Query, UseGuards } from "@nestjs/common";
+import { Controller, Get, Query, UseGuards, UseInterceptors } from "@nestjs/common";
 import { FlowersService } from "./flowers.service";
 import { ParseIntPipe } from "src/conception/pipe";
 import { AuthGuard } from "src/conception/guard";
+import { LoggingInterceptor } from "src/conception/interceptor";
 
 @Controller("flowers")
+@UseInterceptors(LoggingInterceptor)
 export class FlowersController {
     constructor(private readonly flowersService: FlowersService) {}
 
