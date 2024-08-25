a83b3fbd52a08249319f0cafc26990eeaa96051b
Mikail Katsiev <105316523+katsiev1997@users.noreply.github.com>
2024-08-26 00:44:02 +0300

    Config Service

--- src/flowers/flowers.service.ts
+++ src/flowers/flowers.service.ts
@@ -2,12 +2,18 @@ import { Injectable } from "@nestjs/common";
 // import { Flower } from "@prisma/client";
 import { PrismaService } from "src/prisma.service";
 import { FlowersCreateDto } from "./flowers.dto";
+import { ConfigService } from "@nestjs/config";
+import { EnumAppMode } from "src/types";
 
 @Injectable()
 export class FlowersService {
-    constructor(private readonly prisma: PrismaService) {}
+    constructor(
+        private readonly prisma: PrismaService,
+        private readonly configService: ConfigService
+    ) {}
 
     findAll() {
+        console.log(this.configService.get<EnumAppMode>("MODE"));
         return this.prisma.flower.findMany();
         // return [
         //     {
